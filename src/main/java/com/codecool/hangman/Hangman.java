676dd13805573032776a2dfe9457a5bab8b0991b
appuser <appuser@journey2-779c994ccb-wnb7w>
2022-08-08 09:56:01 +0000

    starter project

---
+++ src/main/java/com/codecool/hangman/Hangman.java
@@ -0,0 +1,13 @@
+package com.codecool.hangman;
+
+public class Hangman {
+
+    public static void main(String[] args) {
+        play("Codecool", 6);
+    }
+
+    private static void play(String word, int lives) {
+        // TODO Implement the game here!
+    }
+
+}
